ab38ece935814d5f81d88074c726fbb92d98de09
jingloveing <1172881783@qq.com>
2017-12-28 13:54:49 +0800

    微客官网

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+    $('#showMenu').hover(
+        function () {
+            $('.dowm_menu').addClass('show')
+        },
+        function () {
+            $('.dowm_menu').removeClass('show')
+        }
+    )
